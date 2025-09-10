9f11d00c3b6caa720ef91329b48203e562a2c697
SAKETTTTT <152486217+SAKETTTTT@users.noreply.github.com>
2025-09-10 17:21:54 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,41 +1,42 @@
+# Let us import the Libraries required.
+
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import model_from_json
-from tensorflow.keras.layers import Input
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.utils import register_keras_serializable
 
-# Decorate for serializable fix if needed
-@register_keras_serializable()
-class YourSequential(Sequential):
-    pass
+# To use the model saved in the Json format, We are importing "model_from_json"
+from tensorflow.keras.models import model_from_json
 
 class FacialExpressionModel(object):
 
-    EMOTIONS_LIST = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
+    """ A Class for Predicting the emotions using the pre-trained Model weights"""
 
+    EMOTIONS_LIST = ["Angry", "Disgust",
+                     "Fear", "Happy",
+                     "Neutral", "Sad",
+                     "Surprise"]
+
+    # Whenever we create an instance of class, these are initialized
     def __init__(self, model_json_file, model_weights_file):
+        # Now Let us load model from JSON file which we created during Training
         with open(model_json_file, "r") as json_file:
+            # Reading the json file and storing it in loaded_model
             loaded_model_json = json_file.read()
 
-        # Use custom_objects mapping for 'Sequential'
-        self.loaded_model = model_from_json(
-            loaded_model_json,
-            custom_objects={'Sequential': YourSequential}
-        )
-        
-        # If your model JSON does not include Input layer, rebuild the model with Input layer explicitly:
-        # Example (optional, if needed):
-        # input_layer = Input(shape=(48, 48, 1))
-        # self.loaded_model.build(input_shape=(None, 48, 48, 1))
+        # Load the model architecture from JSON without compiling
+        self.loaded_model = model_from_json(loaded_model_json, compile=False)
 
+        # Explicitly build the model with correct input shape (batch_size=None, 48, 48, 1)
+        self.loaded_model.build(input_shape=(None, 48, 48, 1))
+
+        # Now, Let us load weights into the model
         self.loaded_model.load_weights(model_weights_file)
 
     def predict_emotion(self, img):
-        # Ensure img shape is (1, 48, 48, 1)
+        """ It predicts the Emotion using our pre-trained model and returns it """
         self.preds = self.loaded_model.predict(img)
         return FacialExpressionModel.EMOTIONS_LIST[np.argmax(self.preds)]
 
     def return_probabs(self, img):
+        """ It returns the Probabilities of each emotions using pre-trained model """
         self.preds = self.loaded_model.predict(img)
         return self.preds
