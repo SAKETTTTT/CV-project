488f7fa22e6fa922b99988491df17de733479d4d
SAKETTTTT <152486217+SAKETTTTT@users.noreply.github.com>
2025-09-10 16:28:19 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,41 +1,27 @@
-# Let us import the Libraries required.
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.models import model_from_json, Sequential
 
-# To use the model saved in the Json format, We are importing "model_from_json"
-from tensorflow.keras.models import model_from_json
+from tensorflow.keras.utils import register_keras_serializable
 
+@register_keras_serializable()
+class YourSequential(Sequential):
+    pass
 
 class FacialExpressionModel(object):
+    EMOTIONS_LIST = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
 
-    """ A Class for Predicting the emotions using the pre-trained Model weights"""
-
-    EMOTIONS_LIST = ["Angry", "Disgust",
-                     "Fear", "Happy",
-                     "Neutral", "Sad",
-                     "Surprise"]
-
-    # Whenever we create an instance of class , these are initialized
     def __init__(self, model_json_file, model_weights_file):
-
-        # Now Let us load model from JSON file which we created during Training
         with open(model_json_file, "r") as json_file:
-
-            # Reading the json file and storing it in loaded_model
             loaded_model_json = json_file.read()
-            self.loaded_model = model_from_json(loaded_model_json)
-
-        # Now, Let us load weights into the model
+        # Use custom_objects to map 'Sequential' to your registered class
+        self.loaded_model = model_from_json(loaded_model_json, custom_objects={'Sequential': YourSequential})
         self.loaded_model.load_weights(model_weights_file)
 
     def predict_emotion(self, img):
-        """ It predicts the Emotion using our pre-trained model and returns it """
-
         self.preds = self.loaded_model.predict(img)
         return FacialExpressionModel.EMOTIONS_LIST[np.argmax(self.preds)]
 
     def return_probabs(self, img):
-        """  It returns the Probabilities of each emotions using pre-trained model """
-
         self.preds = self.loaded_model.predict(img)
         return self.preds
